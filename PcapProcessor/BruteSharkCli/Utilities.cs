7ae37159a288d2693d3138b466c9e9522350e377
unknown <oded.shimon89@gmail.com>
2020-03-28 02:50:46 +0300

    Improve Cli

--- PcapProcessor/BruteSharkCli/Utilities.cs
+++ PcapProcessor/BruteSharkCli/Utilities.cs
@@ -10,7 +10,7 @@ namespace BruteSharkCli
 {
     public static class Utilities
     {
-        public static DataTable ToDataTable<T>(this IEnumerable<T> items)
+        public static DataTable ToDataTable<T>(this IEnumerable<T> items, int itemLengthLimit = -1)
         {
             var dataTable = new DataTable(typeof(T).Name);
             PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
@@ -26,7 +26,14 @@ namespace BruteSharkCli
 
                 for (var i = 0; i < props.Length; i++)
                 {
-                    values[i] = props[i].GetValue(item, null);
+                    var cellData = props[i].GetValue(item, null).ToString();
+
+                    if (itemLengthLimit > -1 && cellData.Length > itemLengthLimit)
+                    {
+                        cellData = cellData.Substring(0, itemLengthLimit) + "...";
+                    }
+
+                    values[i] = cellData;
                 }
 
                 dataTable.Rows.Add(values);
@@ -34,5 +41,31 @@ namespace BruteSharkCli
 
             return dataTable;
         }
+
+
+        public static void PrintBruteSharkAsciiArt()
+        {
+            var bruteSharkAscii =@" 
+
+      ,|               ________  ________  ___  ___  _________  _______             ________  ___  ___  ________  ________  ___  __       
+     / ;               |\   __  \|\   __  \|\  \|\  \|\___   ___\\  ___ \          |\   ____\|\  \|\  \|\   __  \|\   __  \|\  \|\  \     
+    /  \               \ \  \|\ /\ \  \|\  \ \  \\\  \|___ \  \_\ \   __/|   ______\ \  \_____ \  \\\  \ \  \|\  \ \  \|\  \ \  \/  /|_   
+   : ,'(                \ \   __  \ \   _  _\ \  \\\  \   \ \  \ \ \  \_|/__|\______\ \_____  \ \   __  \ \   __  \ \   _  _\ \   ___  \  
+   |( `.\                \ \  \|\  \ \  \\  \\ \  \\\  \   \ \  \ \ \  \_|\ \|______|\|____|\  \ \  \ \  \ \  \ \  \ \  \\  \\ \  \\ \  \ 
+   : \  `\       \.       \ \_______\ \__\\ _\\ \_______\   \ \__\ \ \_______\         ____\_\  \ \__\ \__\ \__\ \__\ \__\\ _\\ \__\\ \__\
+    \ `.         | `.      \|_______|\|__|\|__|\|_______|    \|__|  \|_______|        |\_________\|__|\|__|\|__|\|__|\|__|\|__|\|__| \|__|
+     \  `-._     ;   \                                                                \|_________|                                        
+      \     ``-.'.. _ `._
+       `. `-.            ```-...__                                (Network Forensic Analysis Tool)
+        .'`.        --..          ``-..____	
+      ,'.-'`,_-._            ((((   <o.   ,'							
+           `' `-.)``-._-...__````  ____.-'
+               ,'    _,'.--,---------'
+           _.-' _..-'   ),'
+          ``--''        `       ";
+
+            Console.WriteLine(bruteSharkAscii);
+        }
+
     }
 }
