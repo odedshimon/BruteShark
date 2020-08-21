2421a2a4ccbf8c835d216d67769ded50c3f00242
aviad <aviadt15@gmail.com>
2020-08-21 16:20:06 +0300

    WIP started working on udp streams(sessions) reconstruvtion

---
+++ BruteShark/PcapAnalyzer/Objects/UdpStream.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace PcapAnalyzer
+{
+    public class UdpStream
+    {
+        public string SourceIp { get; set; }
+        public string DestinationIp { get; set; }
+        public int SourcePort { get; set; }
+        public int DestinationPort { get; set; }
+        public byte[] Data { get; set; }
+        public List<UdpPacket> Packets { get; set; }
+
+        public UdpStream()
+        {
+            this.Packets = new List<UdpPacket>();
+        }
+
+    }
+}
---
+++ BruteShark/PcapProcessor/Objects/UdpStream.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PcapProcessor
+{
+    public class UdpStream
+    {
+        public string SourceIp { get; set; }
+        public string DestinationIp { get; set; }
+
+        //maybe unneccessary
+        public int SourcePort { get; set; }
+        public int DestinationPort { get; set; }
+        public byte[] Data { get; set; }
+        public List<UdpPacket> Packets { get; set; }
+
+
+        public UdpStream()
+        {
+            this.Packets = new List<UdpPacket>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is UdpStream))
+            {
+                return false;
+            }
+
+            var session = obj as UdpStream;
+
+            // Note: we need to check both directions of the conversation to 
+            // determine equality.
+            return (this.SourceIp == session.SourceIp &&
+                    this.DestinationIp == session.DestinationIp &&
+                    this.SourcePort == session.SourcePort &&
+                    this.DestinationPort == session.DestinationPort) ||
+                    (this.SourceIp == session.DestinationIp &&
+                    this.DestinationIp == session.SourceIp &&
+                    this.SourcePort == session.DestinationPort &&
+                    this.DestinationPort == session.SourcePort);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.SourceIp.GetHashCode() ^
+                   this.SourcePort.GetHashCode() ^
+                   this.DestinationPort.GetHashCode() ^
+                   this.DestinationIp.GetHashCode();
+        }
+
+    }
+}
