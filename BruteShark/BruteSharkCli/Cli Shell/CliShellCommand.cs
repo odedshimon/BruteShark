dec92d120102cdfad5b9977aff261b4c6c095b9d
unknown <oded.shimon89@gmail.com>
2021-01-20 01:20:24 +0200

    WIP

---
+++ BruteShark/BruteSharkCli/Cli Shell/CliShellCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteSharkCli
+{
+    class CliShellCommand
+    {
+        public string Keyword { get; set; }
+        public string Description { get; set; }
+        public Action<string> Action;
+
+        public CliShellCommand(string keyword, Action<string> action, string description = "")
+        {
+            this.Keyword = keyword;
+            this.Description = description;
+            this.Action = action;
+        }
+
+    }
+}
