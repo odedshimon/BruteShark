5a1ce6db616ef8a866168e63a4bf067920dbf7c4
unknown <oded.shimon89@gmail.com>
2021-01-21 23:31:50 +0200

    disable parallel option until merge

--- BruteShark/BruteSharkCli/Single Command Runner/SingleCommandFlags.cs
+++ BruteShark/BruteSharkCli/Single Command Runner/SingleCommandFlags.cs
@@ -7,18 +7,18 @@ namespace BruteSharkCli
 {
     public class SingleCommandFlags
     {
+        [Option('d', "input-dir", Required = false, HelpText = "The input directory containing the files to be processed.")]
+        public string InputDir { get; set; }
+
         [Option('i', "input", Required = false, Separator = ',', HelpText = "The files to be processed seperated by comma")]
         public IEnumerable<string> InputFiles { get; set; }
-
-        [Option('d', "input-dir", Required = false, HelpText = "The input directory containing the files to be processed.")]
-        public string InputDir{ get; set; }
 
         [Option('m', "modules", Required = false , Separator = ',', HelpText = "The modules to be separterd by comma: Credentials, FileExtracting, NetworkMap")]
         public IEnumerable<string> Modules { get; set; }
 
         // TODO - merge parallel processing feature branch to make this flag really work
-        [Option('p', "parallel", Required = false, HelpText = "Whether to process the files in parallel, default value is false.")]
-        public bool ParallelProcessing { get; set; }
+        // [Option('p', "parallel", Required = false, HelpText = "Whether to process the files in parallel, default value is false.")]
+        // public bool ParallelProcessing { get; set; }
 
         [Option('o', "output", Required = false, HelpText = "Output direcorty for the results files.")]
         public string OutputDir { get; set; }
