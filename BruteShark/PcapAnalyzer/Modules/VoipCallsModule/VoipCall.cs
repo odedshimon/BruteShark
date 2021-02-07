2c832a586a8f2b84557391b83c35d02efeaadda1
aviad <aviadt15@gmail.com>
2021-02-07 20:35:58 +0200

    WIP

---
+++ BruteShark/PcapAnalyzer/Modules/VoipCallsModule/VoipCall.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SIPSorcery.SIP;
+using SIPSorcery.Net;
+using System.Linq;
+
+namespace PcapAnalyzer
+{
+    internal class VoipCall
+    {
+        public SIPEndPoint To { get; set; }
+        public SIPEndPoint From { get; set; }
+        public byte[] RTPStream
+        {
+            get => _rtpPackets.SelectMany(p => p.Payload).ToArray();
+        }
+        private List<RTPPacket> _rtpPackets { get; set; }
+        internal VoipCall(SIPEndPoint from, SIPEndPoint to)
+        {
+            this.From = from;
+            this.To = to;
+            _rtpPackets = new List<RTPPacket>();
+        }
+
+        public override bool Equals(object other)
+        {
+            var call = other as VoipCall;
+            return this.From == call.From && this.To == call.To;
+        }
+        public override int GetHashCode()
+        {
+            return this.From.GetHashCode() ^ this.To.GetHashCode();
+        }
+
+        public void addrtpPacket(RTPPacket packet)
+        {
+            _rtpPackets.Add(packet);
+        }
+    }
+}
--- BruteShark/PcapAnalyzer/Modules/VoipCallsModule/VoipCallsModule.cs
+++ BruteShark/PcapAnalyzer/Modules/VoipCallsModule/VoipCallsModule.cs
@@ -1,18 +1,70 @@
+using SIPSorcery.SIP;
+using SIPSorcery.Net;
+using System;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
 
 
 namespace PcapAnalyzer
 
 {
-    public class VoipCallsModule : IModule
+    class VoipCallsModule : IModule
     {
         public string Name => "Voip Calls";
         public event EventHandler<ParsedItemDetectedEventArgs> ParsedItemDetected;
+        public HashSet<VoipCall> VoipCalls = new HashSet<VoipCall>();
 
-        public void Analyze(UdpPacket udpPacket) { }
+        public void Analyze(UdpPacket udpPacket) 
+        {
+            try 
+            {   
+                // try Decode as RTP
+                RTPPacket packet = new RTPPacket(udpPacket.Data);
+            } 
+            catch (Exception ex) { }
+            
+            try
+            {
+                // try Decode as SIP request
+                SIPRequest sipRequest = tryDecodeSipRequest(udpPacket);
+                AddCall(sipRequest);
+            }
+            catch (Exception ex)
+            { 
+                // try Decode as SDP response
+                SDP sdpMessage = SDP.ParseSDPDescription(Encoding.UTF8.GetString(udpPacket.Data));
+            }
+
+            try
+            {
+                // try Decode as SIP response
+                SIPResponse sipResponse = tryDecodeSipResponse(udpPacket);
+                AddCall(sipResponse);
+            }
+            catch (Exception ex) {}
+        }
 
         public void Analyze(TcpPacket tcpPacket) { }
 
         public void Analyze(TcpSession tcpSession) {}
         public void Analyze(UdpStream udpStream) {}
+
+        private SIPRequest tryDecodeSipRequest(UdpPacket udpPacket)
+        {
+            return SIPRequest.ParseSIPRequest(Encoding.UTF8.GetString(udpPacket.Data));
+        }
+        private SIPResponse tryDecodeSipResponse(UdpPacket udpPacket)
+        {
+            return SIPResponse.ParseSIPResponse(Encoding.UTF8.GetString(udpPacket.Data));
+        }
+        private bool AddCall(SIPMessageBase sipMessage)
+        {
+            var call = new VoipCall(sipMessage.LocalSIPEndPoint, sipMessage.RemoteSIPEndPoint);
+            if(sipMessage.GetType() is SIPResponse)
+            {
+            }
+            return VoipCalls.Add(call);
+        }
     }
 }
